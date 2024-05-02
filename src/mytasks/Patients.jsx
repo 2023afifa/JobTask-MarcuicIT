b2e47ad8fce9bcae8126dcedf0755c4880d8a0a2
afifa <2023afifa@gmail.com>
2024-05-02 18:09:54 +0600

    Modified Patients page and removed unused CSS file

--- src/mytasks/Patients.jsx
+++ src/mytasks/Patients.jsx
@@ -1,9 +1,80 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
+import { records as data } from "../pages/tables/data";
+import Table from "./Table";
+import { FaFileContract } from "react-icons/fa";
+
+
+const columns = [
+    {
+        Header: "ID",
+        accessor: "id",
+        sort: true,
+    },
+    {
+        Header: "Name",
+        accessor: "name",
+        sort: true,
+    },
+    {
+        Header: "Phone Number",
+        accessor: "phone",
+        sort: false,
+    },
+    {
+        Header: "Age",
+        accessor: "age",
+        sort: true,
+    },
+    {
+        Header: "Company",
+        accessor: "company",
+        sort: false,
+    },
+];
+
+
+const sizePerPageList = [
+    {
+        text: "5",
+        value: 5,
+    },
+    {
+        text: "10",
+        value: 10,
+    },
+    {
+        text: "25",
+        value: 25,
+    },
+    {
+        text: "All",
+        value: data.length,
+    },
+];
+
+
 
 const Patients = () => {
     return (
         <div>
-            <h2>Patients</h2>
+            <h3 className='d-flex align-items-center mt-3'>
+                <FaFileContract />Patients
+            </h3>
+            <Card style={{ borderRadius: '15px', borderTopWidth: "8px", borderTopColor: "#007bff" }}>
+                <Card.Body>
+                    <h4>Patients Table</h4>
+                    <Table
+                        columns={columns}
+                        data={data}
+                        pageSize={5}
+                        sizePerPageList={sizePerPageList}
+                        isSortable={true}
+                        pagination={true}
+                        isSearchable={true}
+                    />
+                </Card.Body>
+            </Card>
         </div>
     );
 };
